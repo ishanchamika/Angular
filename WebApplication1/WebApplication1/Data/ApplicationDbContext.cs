2537c147c3ce7adffafff6e9cde1d25190738558
ishanchamika <ishanchami9@gmail.com>
2025-01-24 11:19:48 +0530

    initial commit Angular and .Net

---
+++ WebApplication1/WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.Entities;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> MyProperty { get; set; } 
+    }
+}
